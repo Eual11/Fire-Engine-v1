5f8515ccb40299ed3d72dc49831735e860441342
Eual11 <uchihaeual11@gmail.com>
2024-05-22 11:20:19 +0300

    optimizing stuff

--- src/test2.cpp
+++ src/test2.cpp
@@ -9,13 +9,14 @@
 #include <algorithm>
 
 #include <stdint.h>
+#include <uml/mat4x4.h>
 #include <uml/transform.h>
 #include <uml/vec2.h>
 #include <uml/vec3.h>
 #include <uml/vec4.h>
 #include <vector>
-const int WINDOW_WIDTH = 640;
-const int WINDOW_HEIGHT = 480;
+constexpr int WINDOW_WIDTH = 640;
+constexpr int WINDOW_HEIGHT = 480;
 SDL_Window *gWindow = nullptr;
 SDL_Renderer *gRenderer = nullptr;
 TTF_Font *gFont = nullptr;
@@ -35,10 +36,10 @@ int main(int argc, char **argv) {
 
   mesh cube;
   cube.LoadObj("./assets/objects/teapot.obj");
-  float zNear = 0.1;
-  float zFar = 9;
-  float t = 80.0 * (3.1415 / 180);
-  float fFov = t;
+  constexpr float zNear = 0.1;
+  constexpr float zFar = 9;
+  constexpr float t = 80.0 * (3.1415 / 180);
+  constexpr float fFov = t;
 
   float fYaw = 0.0f;
   float fTargetYaw = 0.0f;
@@ -97,16 +98,14 @@ int main(int argc, char **argv) {
       }
     }
     mat4x4 ry = RotateY(fYaw);
-    mat4x4 rx = RotateX(fYaw);
+    mat4x4 rx = RotateX(0.0);
     mat4x4 rz = RotateZ(fBank);
-    mat4x4 sc = ScaleUniform(1);
+    /* mat4x4 sc = ScaleX(0.5); */
     vec3 target = {0, 0, 1};
     target = target * RotateY(fTargetYaw);
-    lookDir = target - cameraPosition;
-    lookDir.normalize();
+    mat4x4 final_transform = rx * ry * rz;
     mat4x4 viewMatrix =
         LookAt(cameraPosition, target + cameraPosition, {0, 1, 0});
-
     std::vector<triangle> projectedMesh;
     for (auto tri : cube.triangles) {
 
@@ -115,8 +114,7 @@ int main(int argc, char **argv) {
       // transforming each vertex of the triangle
       for (size_t i = 0; i < 3; i++) {
 
-        vec3 pos = tri.verticies[i].position;
-        tri.verticies[i].position = pos * rz * rx * ry;
+        tri.verticies[i].position = tri.verticies[i].position * final_transform;
         tri.verticies[i].position.z += 11;
 
         // view transform goes here
@@ -133,7 +131,7 @@ int main(int argc, char **argv) {
       vec3 q1 = tri.verticies[2].position - tri.verticies[0].position;
       vec3 normal = Cross(q0, q1);
       normal.normalize();
-
+      // TODO: fix this
       float theta = normal * lookDir;
 
       if (theta < 0) {
@@ -141,33 +139,31 @@ int main(int argc, char **argv) {
 
           // view transform
           //
-          vec3 pos = tri.verticies[i].position;
-          tri.verticies[i].position = pos * viewMatrix;
+          tri.verticies[i].position = tri.verticies[i].position * viewMatrix;
         }
 
         std::vector<triangle> clippedTriangles =
             PlaneClipTriangle(lookDir, {0, 0, 0.5}, tri);
+        triangle projectedTriangle;
         for (auto &clippedTri : clippedTriangles) {
-          triangle projectedTriangle;
           for (size_t i = 0; i < 3; i++) {
             vec4 projectedVec = vec4{clippedTri.verticies[i].position, 1};
             projectedVec = projectedVec * clipMatrix;
             // prespective divide
             //
-            projectedVec.x /= projectedVec.w;
-            projectedVec.y /= projectedVec.w;
-            projectedVec.z /= projectedVec.w;
+            if (projectedVec.w != 0.0f) {
+              projectedVec.x /= projectedVec.w;
+              projectedVec.y /= projectedVec.w;
+              projectedVec.z /= projectedVec.w;
+            }
 
-            vertex projectedVertex = {projectedVec.x, projectedVec.y,
-                                      projectedVec.z};
+            vertex projectedVertex;
+            projectedVertex.position.x = projectedVec.x;
+
+            projectedVertex.position.y = projectedVec.y;
+            projectedVertex.position.z = projectedVec.z;
             projectedTriangle.verticies[i] = projectedVertex;
           }
-          vec3 q0 = clippedTri.verticies[1].position -
-                    clippedTri.verticies[0].position;
-          vec3 q1 = clippedTri.verticies[2].position -
-                    clippedTri.verticies[0].position;
-          vec3 normal = Cross(q0, q1);
-          normal.normalize();
 
           projectedTriangle.normal = normal;
           projectedMesh.push_back(projectedTriangle);
@@ -194,12 +190,12 @@ int main(int argc, char **argv) {
 
     size_t projectedMeshTriCount = projectedMesh.size();
     vertexToRender.resize(projectedMeshTriCount * 3);
-    int k = 0;
+    size_t k = 0;
     for (size_t i = 0; i < projectedMeshTriCount; i++) {
-      triangle tri = projectedMesh[i];
+      const triangle &tri = projectedMesh[i];
 
       for (int j = 0; j < 3; j++) {
-        vertex vert = tri.verticies[j];
+        const vertex &vert = tri.verticies[j];
         SDL_Vertex finalvert;
 
         // calculating basic lambertain lighting using per-triangle normals,
@@ -222,7 +218,7 @@ int main(int argc, char **argv) {
                        vertexToRender.size(), nullptr, 0);
     fpsTimer->displayFPS();
     SDL_RenderPresent(gRenderer);
-    /* fYaw += 0.02; */
+    fYaw += 0.02;
     /* quit = true; */
     /* fBank += 0.12; */
     /* fPitch += 0.009; */
