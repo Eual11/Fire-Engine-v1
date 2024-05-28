51ae7b809a6b0fe13c957912b73eaa17bff38d1b
Eual11 <uchihaeual11@gmail.com>
2024-05-28 10:50:46 +0300

    better lights

--- include/FireEngine/Light.hpp
+++ include/FireEngine/Light.hpp
@@ -4,15 +4,27 @@
 #include "premitives.hpp"
 
 namespace FireEngine {
-struct DirectionalLight {
+
+// TODO: a better way to represent lights and more types of lights
+enum class LIGHT_TYPE { EMISSIVE, DIRECTIONAL, SPOT, POINT, UNDEFINED };
+struct Light {
+
+  vec3 color;
+  float intensity;
+  LIGHT_TYPE type = LIGHT_TYPE::UNDEFINED;
+  Light(const vec3 &_col, float _intensity)
+      : color{_col}, intensity{_intensity} {}
+};
+struct DirectionalLight : public Light {
 
   vec3 direction;
   vec3 color;
   float intensity;
 
-  DirectionalLight() = default;
   DirectionalLight(vec3 _direction, vec3 _color, float _intensity)
-      : direction{_direction}, color{_color}, intensity{_intensity} {}
+      : Light{_color, _intensity}, direction{_direction} {
+    type = LIGHT_TYPE::DIRECTIONAL;
+  }
   inline void setDirection(const vec3 &_direction) { direction = _direction; }
   inline void setColor(const vec3 &_color) { color = _color; }
   inline void setIntensity(float _intensity) {
